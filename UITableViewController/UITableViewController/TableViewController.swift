4846413b689c3d6e7fcd6f5f58537aa2a89ca1c5
Ariunjargal Batjargal <abatjarg@icloud.com>
2020-06-21 17:31:10 -0400

    Added in table view sample app

---
+++ UITableViewController/UITableViewController/TableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewController.swift
+//  UITableViewController
+//
+//  Created by abatjarg on 6/21/20.
+//  Copyright © 2020 abatjarg. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setting the background color to white in the main container view
+        view.backgroundColor = .white
+    }
+}
