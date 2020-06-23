60f830403e31946b1e3df378e389b59fc5136569
Ariunjargal Batjargal <abatjarg@icloud.com>
2020-06-22 20:42:38 -0400

    Added in table view sample app

--- UITableViewController/UITableViewController/TableViewController.swift
+++ UITableViewController/UITableViewController/TableViewController.swift
@@ -10,10 +10,42 @@ import UIKit
 
 class TableViewController: UIViewController {
     
+    let name = ["Honda", "Toyota", "BMW"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Setting the background color to white in the main container view
-        view.backgroundColor = .white
+        let tableView = UITableView()
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+    }
+}
+
+extension TableViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = name[indexPath.row]
+        return cell
+    }
+    
+    
+}
+
+extension TableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected: \(name[indexPath.row])")
     }
 }
