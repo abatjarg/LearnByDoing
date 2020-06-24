e21ddd49027704476c151a3629ab1ba8052a5b2f
Ariunjargal Batjargal <abatjarg@icloud.com>
2020-06-23 23:54:37 -0400

    Added in collection view sample app

---
+++ UICollectionViewController/UICollectionView/UICollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionView
+//
+//  Created by abatjarg on 6/23/20.
+//  Copyright © 2020 abatjarg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
